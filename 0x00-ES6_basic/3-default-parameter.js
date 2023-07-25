a0d17b873b7c3734ac8cfd809cd1308f0a1ad753
hopedavids <hledavids@gmail.com>
2023-07-25 23:55:46 +0000

    ecmascript optimzation

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+"use strict";
+
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
