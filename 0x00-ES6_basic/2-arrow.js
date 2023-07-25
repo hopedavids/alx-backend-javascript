5bbb401c354683cd1a58100cefe567f8fdf34261
hopedavids <hledavids@gmail.com>
2023-07-25 23:48:36 +0000

    ecmascript arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -3,7 +3,8 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-
+  const self = this;
+  
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
