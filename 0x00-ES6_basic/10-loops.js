82d89e263c0f44143ff5f2c48ad84170a6ba4e9c
hopedavids <hledavids@gmail.com>
2023-07-26 10:56:41 +0000

    loops  in ecmascript

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (let value of array) {
+    value = appendString + value;
+  }
+
+  return array;
+}
+
