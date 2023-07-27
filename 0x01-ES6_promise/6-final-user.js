34d054dab431418615c6763fc29e6c293c94e700
Hope Davids <hopeolawole@gmail.com>
2023-07-27 13:16:10 +0000

    modified the ecmascript

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,26 +1,19 @@
-#!/usr/bin/node
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-// 6-handle-profile.js
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-export function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpPromise, uploadPromise]).then((results) => {
-    return results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: 'fulfilled',
-          value: result.value,
-        };
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
       } else {
-        return {
-          status: 'rejected',
-          value: result.reason.message,
-        };
+        result.push({ status: element.status, value: `${element.reason}` });
       }
     });
+    return result;
   });
 }
