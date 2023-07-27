af39c37efb43cdb7f38e134fd22aa3cc360113bb
Hope Davids <hopeolawole@gmail.com>
2023-07-27 23:03:44 +0000

    create three new objects from the class blueprint

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
 
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms(){
+    const roomSizes = [19, 20, 34];
+    const classrooms = roomSizes.map(size => new ClassRoom(size));
+    return classrooms;
+}
