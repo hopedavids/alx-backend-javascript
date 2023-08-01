ed23cb211680e4aec36446d09d32d71854b4d2ee
Hope Davids <hopeolawole@gmail.com>
2023-08-01 14:53:27 +0000

    using reduce in an array

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+export default function getStudentIdsSum(students){
+    const sum = students.reduce((accumulator, student) => accumulator + student.id, 0);
+    
+    return sum;
+}
