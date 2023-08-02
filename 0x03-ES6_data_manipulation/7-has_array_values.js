9f8094143008b6994f988e851f5e5c4afd858857
Hope Davids <hopeolawole@gmail.com>
2023-08-02 12:53:42 +0000

    iterating using for loop

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+export default function hasValuesFromArray(set, array){
+    for (const element of array){
+        if (!set.has(element)){
+            return false;
+        }
+    }
+
+    return true;
+
+}
