ccd78b22f9626c893a7dde3f4302a10f6e2181c2
Hope Davids <hopeolawole@gmail.com>
2023-08-02 13:34:43 +0000

    update unique items

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function updateUniqueItems(groceriesMap) {
+    if (!(groceriesMap instanceof Map)) {
+      throw new Error('Cannot process');
+    }
+  
+    groceriesMap.forEach((quantity, item) => {
+      if (quantity === 1) {
+        groceriesMap.set(item, 100);
+      }
+    });
+  }
+  
+  export default updateUniqueItems;
+  
