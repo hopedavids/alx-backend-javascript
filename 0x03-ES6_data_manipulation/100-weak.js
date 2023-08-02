8c64f65732a699463247b05fef62ac1180d18684
Hope Davids <hopeolawole@gmail.com>
2023-08-02 13:39:30 +0000

    leveraging weak in ecmascript6

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// 100-weak.js
+
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+        const count = weakMap.get(endpoint);
+    if (count >= 5) {
+
+        throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, count + 1);
+  }
+}
